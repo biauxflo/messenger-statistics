ccd9beebbd91b9e05c9ddebde6626587b8df7f88
Florestan Biaux <florestan.biaux@etu.utc.fr>
2021-07-21 11:06:48 +0200

    base du main

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import tri
 
 if __name__ == "__main__":
-    tri.tri()
+#demander le nom de la conv à analyser
+    nom = input("Quel est le nom de la conversation à analyser ?")
+#recuperer bonne conversation
+    #conv = recup(nom)
--- tri.py
+++ tri.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 def tri():
     print("Hello tri\n")
