422c595dfb4ed55361d283ec6e71275f6385480f
Adrien Simon <adrien.simon@etu.utc.fr>
2021-07-21 23:43:54 +0200

    possibilité de changer de conversation dans le menu

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ if __name__ == "__main__":
     data = get.getfile(name)  # data = json
     # choose action to do
     choice = 0
-    while choice != 5:
-        choice = int(input("\n1 - Afficher les messages\n2 - Afficher les participants\n3 - Afficher les statistiques\n4 - Afficher les reactions\n5 - Quitter l\'application\n"))
+    while choice != 6:
+        choice = int(input("\n1 - Afficher les messages\n2 - Afficher les participants\n3 - Afficher les statistiques\n4 - Afficher les reactions\n5 - Changer de conversation\n6 - Quitter l\'application\n"))
         if choice == 1:
             print(data)
         if choice == 2:
@@ -24,3 +24,6 @@ if __name__ == "__main__":
             stats.printtab(stats.counttalk(users.usertab(data), data))
         if choice == 4:
             stats.countreacts(data)
+        if choice == 5:
+            name = str(input("Quel est le nom de la conversation à analyser ? \n"))
+            data = get.getfile(name)
