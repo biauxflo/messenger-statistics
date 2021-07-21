eef7149a63685eec9f9b758f0c6ab4fee23b11c6
Florestan Biaux <florestan.biaux@etu.utc.fr>
2021-07-21 11:17:11 +0200

    translation

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import tri
-import recup
+import get
 
 if __name__ == "__main__":
-#demander le nom de la conv à analyser
-    nom = input("Quel est le nom de la conversation à analyser ?")
-#recuperer bonne conversation
-    conv = recup.recup(nom)
+# demander le nom de la conv à analyser
+    name = input("Quel est le nom de la conversation à analyser ?")
+# recuperer bonne conversation
+    conv = get.get(name)
