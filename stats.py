fcceaf1a75056a3543b075affa5bc42d480c8c03
Florestan Biaux <florestan.biaux@etu.utc.fr>
2021-07-21 23:17:53 +0200

    correction affichage stats

--- stats.py
+++ stats.py
@@ -2,19 +2,19 @@ import decode
 
 def counttalk(usertab, data):
     tmp = ""
-    print(len(usertab))
-    print("\n")
     for i in range(len(usertab)):
         tmp = tmp + "0 "
     tmp = tmp.split()
     counttalk = [usertab, tmp]
     messages = data["messages"]
+    cpt = 0
     for msg in messages:
         for i in range(len(counttalk[0])):
             if decode.string_decode(msg["sender_name"]) == counttalk[0][i]:
                 counttalk[1][i] = int(counttalk[1][i])+1
+                cpt += 1
+    print("Messages envoyés : " + str(cpt) + " messages\n")
     return counttalk
-
 
 def printtab(tab):
     for i in range(len(tab[0])):
