81e3c2c29f9c024bd4a322d251a7724b55f39e32
Florestan Biaux <florestan.biaux@etu.utc.fr>
2021-07-21 11:14:32 +0200

    MaJ SDK + ajout recup.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import tri
+import recup
 
 if __name__ == "__main__":
 #demander le nom de la conv à analyser
     nom = input("Quel est le nom de la conversation à analyser ?")
 #recuperer bonne conversation
-    #conv = recup(nom)
+    conv = recup.recup(nom)
