dc31260f5860313756bf54dce4266780859ad3eb
Adrien Simon <adrien.simon@etu.utc.fr>
2021-07-30 23:18:42 +0200

    Passage à une IHM Ancien menu console sauvegardé dans oldmenu.py Fenetre basique crée

--- main.py
+++ main.py
@@ -1,51 +1,20 @@
-import get
-import users
-import stats
-import os
-from tkinter import Tk
+import tkinter as tk
 
+class App(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__( master )
+        self.master = master
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        pass
 
 if __name__ == "__main__":
-    root = Tk()
-    root.mainloop()
-    # demander le nom de la conv à analyser
-    originpath = os.getcwd()
-    name = '.'
-    # choose action to do
-    choice = 0
-    while choice != 10:
-        if name == '.':
-            choice = 5
-        else :
-            choice = input("\n1 - Afficher les messages\n2 - Afficher les participants\n3 - Afficher les statistiques\n4 - Afficher les reactions\n5 - Changer de conversation\n6 - Nombre de messages supprimés\n7 - Classement des mots\n8 - Occurences d'un mot spécifique\n9 - Dernière parole d\'un membre\n10 - Quitter l\'application\n")
-            try:
-                choice = int(choice)
-            except ValueError:
-                choice = 0
-        if choice == 1:
-            print(data)
-        if choice == 2:
-            print(users.usertab(data))
-        if choice == 3:
-            stats.counttalk(data)
-        if choice == 4:
-            stats.countreacts(data)
-        if choice == 5:
-            name = '.'
-            while name == '.':
-                name = str(input( "\nQuel est le nom de la conversation à analyser ? \n"))
-                # get json file
-                try:
-                    data = get.getfile( name )  # data = json
-                except IndexError:
-                    os.chdir( originpath )
-                    print( "Désolé, Conversation non trouvée." )
-                    name = '.'
-        if choice == 6:
-            stats.countunset(data)
-        if choice == 7:
-            stats.countwords(data)
-        if choice == 8:
-            stats.countspecificword(data)
-        if choice == 9:
-            stats.lasttalk(data)
+    root = tk.Tk()
+    main = App(master=root)
+
+    main.master.title("Messenger Statistics")
+    main.master.minsize(640, 400)
+
+    main.mainloop()
---
+++ oldmenu.py
@@ -0,0 +1,47 @@
+import get
+import users
+import stats
+import os
+
+# demander le nom de la conv à analyser
+if __name__ == "__main__":
+    originpath = os.getcwd()
+    name = '.'
+    # choose action to do
+    choice = 0
+    while choice != 10:
+        if name == '.':
+            choice = 5
+        else :
+            choice = input("\n1 - Afficher les messages\n2 - Afficher les participants\n3 - Afficher les statistiques\n4 - Afficher les reactions\n5 - Changer de conversation\n6 - Nombre de messages supprimés\n7 - Classement des mots\n8 - Occurences d'un mot spécifique\n9 - Dernière parole d\'un membre\n10 - Quitter l\'application\n")
+            try:
+                choice = int(choice)
+            except ValueError:
+                choice = 0
+        if choice == 1:
+            print(data)
+        if choice == 2:
+            print(users.usertab(data))
+        if choice == 3:
+            stats.counttalk(data)
+        if choice == 4:
+            stats.countreacts(data)
+        if choice == 5:
+            name = '.'
+            while name == '.':
+                name = str(input( "\nQuel est le nom de la conversation à analyser ? \n"))
+                # get json file
+                try:
+                    data = get.getfile( name )  # data = json
+                except IndexError:
+                    os.chdir( originpath )
+                    print( "Désolé, Conversation non trouvée." )
+                    name = '.'
+        if choice == 6:
+            stats.countunset(data)
+        if choice == 7:
+            stats.countwords(data)
+        if choice == 8:
+            stats.countspecificword(data)
+        if choice == 9:
+            stats.lasttalk(data)
