2b3acbaefa181757713d486f1eaea9a145dddd1d
florestan <florestan.biaux@etu.utc.fr>
2021-10-19 14:20:08 +0200

    Update file name to run without pbls

---
+++ dev.py
@@ -0,0 +1,52 @@
+import tkinter as tk
+
+class App(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widget()
+
+    def create_widget(self):
+        text = tk.Label(self, text="DEV EN COURS")
+        text.pack(pady=50)
+
+        bouton = tk.Button(self, text="Dis Bonjour", command=self.hello_world, padx=10, pady=10)
+        bouton.pack()
+        pass
+
+    def hello_world(self):
+        print("Hello World !")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    main = App(master=root)
+
+    main.master.title("Messenger Statistics")
+    main.master.iconbitmap("img/messenger_stats.ico")
+    main.master.minsize(640, 400)
+
+    menubar = tk.Menu(main)
+    main.master.config(menu=menubar)
+
+    menufichier = tk.Menu(menubar, tearoff=0)
+    menubar.add_cascade(label="Fichier", menu=menufichier)
+    menufichier.add_command(label="Ouvrir")
+    menufichier.add_command(label="Fermer")
+
+    menuaffichage = tk.Menu(menubar, tearoff=0)
+    menubar.add_cascade(label="Affichage", menu=menuaffichage)
+    menuaffichage.add_command(label="Grille")
+    menuaffichage.add_command(label="Rows")
+    menuaffichage.add_command(label="Cols")
+
+    menustats = tk.Menu(menubar, tearoff=0)
+    menubar.add_cascade(label="Stats", menu=menustats)
+    sousmenucascade = tk.Menu(menustats, tearoff=0)
+    menustats.add_cascade(label="Messages", menu=sousmenucascade)
+    sousmenucascade.add_command(label="Nombre de messages")
+    sousmenucascade.add_command(label="Messages supprimés")
+    menustats.add_command(label="Emojis")
+    menustats.add_command(label="Mots")
+
+    main.mainloop()
--- main.py
+++ main.py
@@ -1,52 +1,47 @@
-import tkinter as tk
+import get
+import users
+import stats
+import os
 
-class App(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
-        self.pack()
-        self.create_widget()
-
-    def create_widget(self):
-        text = tk.Label(self, text="DEV EN COURS")
-        text.pack(pady=50)
-
-        bouton = tk.Button(self, text="Dis Bonjour", command=self.hello_world, padx=10, pady=10)
-        bouton.pack()
-        pass
-
-    def hello_world(self):
-        print("Hello World !")
-
+# demander le nom de la conv à analyser
 if __name__ == "__main__":
-    root = tk.Tk()
-    main = App(master=root)
-
-    main.master.title("Messenger Statistics")
-    main.master.iconbitmap("img/messenger_stats.ico")
-    main.master.minsize(640, 400)
-
-    menubar = tk.Menu(main)
-    main.master.config(menu=menubar)
-
-    menufichier = tk.Menu(menubar, tearoff=0)
-    menubar.add_cascade(label="Fichier", menu=menufichier)
-    menufichier.add_command(label="Ouvrir")
-    menufichier.add_command(label="Fermer")
-
-    menuaffichage = tk.Menu(menubar, tearoff=0)
-    menubar.add_cascade(label="Affichage", menu=menuaffichage)
-    menuaffichage.add_command(label="Grille")
-    menuaffichage.add_command(label="Rows")
-    menuaffichage.add_command(label="Cols")
-
-    menustats = tk.Menu(menubar, tearoff=0)
-    menubar.add_cascade(label="Stats", menu=menustats)
-    sousmenucascade = tk.Menu(menustats, tearoff=0)
-    menustats.add_cascade(label="Messages", menu=sousmenucascade)
-    sousmenucascade.add_command(label="Nombre de messages")
-    sousmenucascade.add_command(label="Messages supprimés")
-    menustats.add_command(label="Emojis")
-    menustats.add_command(label="Mots")
-
-    main.mainloop()
+    originpath = os.getcwd()
+    name = '.'
+    # choose action to do
+    choice = 0
+    while choice != 10:
+        if name == '.':
+            choice = 5
+        else :
+            choice = input("\n1 - Afficher les messages\n2 - Afficher les participants\n3 - Afficher les statistiques\n4 - Afficher les reactions\n5 - Changer de conversation\n6 - Nombre de messages supprimés\n7 - Classement des mots\n8 - Occurences d'un mot spécifique\n9 - Dernière parole d\'un membre\n10 - Quitter l\'application\n")
+            try:
+                choice = int(choice)
+            except ValueError:
+                choice = 0
+        if choice == 1:
+            print(data)
+        if choice == 2:
+            print(users.usertab(data))
+        if choice == 3:
+            stats.counttalk(data)
+        if choice == 4:
+            stats.countreacts(data)
+        if choice == 5:
+            name = '.'
+            while name == '.':
+                name = str(input( "\nQuel est le nom de la conversation à analyser ? \n"))
+                # get json file
+                try:
+                    data = get.getfile( name )  # data = json
+                except IndexError:
+                    os.chdir( originpath )
+                    print( "Désolé, Conversation non trouvée." )
+                    name = '.'
+        if choice == 6:
+            stats.countunset(data)
+        if choice == 7:
+            stats.countwords(data)
+        if choice == 8:
+            stats.countspecificword(data)
+        if choice == 9:
+            stats.lasttalk(data)
